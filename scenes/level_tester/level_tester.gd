4816de9053d4e656c75ff8550e68c3608bab4500
Jan Seda <sedajancz@gmail.com>
2024-11-25 01:49:01 +0100

    add more characters in LevelTester

--- scenes/level_tester/level_tester.gd
+++ scenes/level_tester/level_tester.gd
@@ -8,6 +8,19 @@ extends Node
 @onready var _fps_label := $Fps as Label
 
 
+func mk_chr() -> GameCharacter:
+	var test_char2 := PlayableCharacter.new()
+	test_char2.name = "Test Character 2"
+	test_char2.portrait = "res://resources/portraits/placeholder_2.png"
+	test_char2.hair = preload("res://models/hair0v2.glb")
+	test_char2.hair_color = Color.DARK_GOLDENROD
+	test_char2.equipment.add_entity(WeaponRef.new(preload("res://game_resources/playground/short_sword.tres")), ItemEquipment.Slot.MAIN)
+	var prof := TalentProficiency.new()
+	prof.l1_types.append(WeaponMeta.TypeL1Id.MELEE)
+	test_char2.available_talents.add_entity(TalentPack.new([prof]))
+	return test_char2
+
+
 func _ready() -> void:
 	global.PLAYER_STATE_PATH = $PlayerState.get_path()
 	global.CONTROLLED_CHARACTERS_PATH = level.get_node("ControlledCharacters").get_path()
@@ -15,18 +28,16 @@ func _ready() -> void:
 	test_char.name = "Test Character"
 	test_char.hair = preload("res://models/hair0v2.glb")
 	test_char.hair_color = Color.FOREST_GREEN
-	var test_char2 := PlayableCharacter.new()
-	test_char2.name = "Test Character 2"
-	test_char2.hair = preload("res://models/hair0v2.glb")
-	test_char2.hair_color = Color.DARK_GOLDENROD
-	level.spawn_playable_characters([test_char, test_char2])
-
+	var initiative_bonus := TalentSkillBonus.new()
+	initiative_bonus.skill = Skills.INITIATIVE
+	initiative_bonus.amount = 20
+	var in_bonus_pack := TalentPack.new([initiative_bonus])
+	test_char.talents.add_entity(in_bonus_pack)
+	test_char.available_talents.add_entity(in_bonus_pack)
 	test_char.equipment.add_entity(WeaponRef.new(preload("res://game_resources/playground/sparky_sword.tres")), ItemEquipment.Slot.MAIN)
 	test_char.set_attribute(CharacterAttributes.WILL, 2)
-	test_char2.equipment.add_entity(WeaponRef.new(preload("res://game_resources/playground/short_sword.tres")), ItemEquipment.Slot.MAIN)
-	var prof := TalentProficiency.new()
-	prof.l1_types.append(WeaponMeta.TypeL1Id.MELEE)
-	test_char2.available_talents.add_entity(TalentPack.new([prof]))
+
+	level.spawn_playable_characters([test_char, mk_chr(), mk_chr(), mk_chr()])
 
 	global.player_state().inventory.add_entity(WeaponRef.new(preload("res://game_resources/playground/sparky_sword.tres")))
 	global.player_state().inventory.add_entity(StackRef.new(preload("res://game_resources/playground/mist_shard.tres")))
@@ -42,8 +53,8 @@ func _ready() -> void:
 	global.player_state().inventory.add_entity(StackRef.new(preload("res://game_resources/playground/mist_shard.tres")))
 	global.player_state().inventory.add_entity(StackRef.new(preload("res://game_resources/playground/mist_shard.tres")))
 
-	test_char.fill_ability_bar()
-	test_char2.fill_ability_bar()
+	for chara in (level.get_node("ControlledCharacters") as ControlledCharacters).get_characters():
+		chara.fill_ability_bar()
 
 	if disable_fow:
 		var fow := get_parent().find_child("RustyFow") as RustyFow
