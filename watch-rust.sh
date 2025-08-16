8ccc624c4da4e377a3c5e909c790409f242fa786
Jan Seda <sedajancz@gmail.com>
2025-08-16 15:03:29 +0200

    move everything into game subdir
    So not everything is part of the godot project
    
    And also include the models.blend file with relative export paths

--- watch-rust.sh
+++ watch-rust.sh
@@ -1,2 +1,2 @@
 # Re-build rust libraries whenever rust source code changes
-(cd rust && cargo build && while inotifywait -e close_write src/**/*.rs; do cargo build; done)
+(cd game/rust && cargo build && while inotifywait -e close_write src/**/*.rs; do cargo build; done)
